be67654a73f735cf7171619feb9417f11b99735b
Muslim Guseinov <gusejnov1999@list.ru>
2018-12-15 23:56:21 +0300

    update(suspenceEnhancer): render loader from atom

--- src/hocs/suspenseEnhancer.tsx
+++ src/hocs/suspenseEnhancer.tsx
@@ -1,14 +1,11 @@
 import React, { Suspense } from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { Layout } from '../ui/atoms';
+import { Loader } from '../ui/atoms';
 
 export default function suspenseEnhancer<OriginProps>(C: React.ComponentType<OriginProps & any>) {
 	return (props: any) => (
 		<Suspense
 			fallback={(
-				<Layout flow="row" height="100vh" justify="center" align="center">
-					<CircularProgress disableShrink />;
-				</Layout>
+				<Loader />
 			)}
 		>
 			<C
