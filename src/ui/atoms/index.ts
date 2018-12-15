4d95d9fd8e6a1ba6524e723c02a3d1027bf8f3b9
Muslim Guseinov <gusejnov1999@list.ru>
2018-12-15 23:53:47 +0300

    create(Ui): root export

--- src/ui/atoms/index.ts
+++ src/ui/atoms/index.ts
@@ -1,3 +1,5 @@
 export { Layout } from './Layout';
 export { FieldText } from './FieldText';
 export { Link } from './Link';
+export { NavLink } from './NavLink';
+export { Loader } from './Loader';
