f1a77758c0317849f075bd834514b763676f6822
Muslim Guseinov <gusejnov1999@list.ru>
2018-12-15 23:47:22 +0300

    feature(signup)

--- src/modules/signup/index.tsx
+++ src/modules/signup/index.tsx
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
-import { Field, Form } from 'react-final-form';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import LockIcon from '@material-ui/icons/LockOutlined';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
-import { FieldText, Link } from '../../ui/atoms';
-import styles from './styles';
+
+import SignUpPage from '../../ui/pages/SignupPage';
+import { SignUpRequest } from '../../@types';
 
 interface IProps {
 	classes: any;
@@ -29,67 +22,20 @@ const signUpMutation: any = gql`
 // @ts-ignore
 @graphql(signUpMutation)
 class SignUp extends Component<IProps, any> {
-	onSubmit = async (values: any) => {
+	onSubmit = async (values: SignUpRequest) => {
 		await this.props.mutate({
 			variables: values,
 		});
 	}
 
 	render() {
-		const { classes } = this.props;
 
 		return (
-			<main className={classes.main}>
-				<CssBaseline/>
-				<Paper className={classes.paper}>
-					<Avatar className={classes.avatar}>
-						<LockIcon/>
-					</Avatar>
-					<Typography component="h1" variant="h5">
-						Sign Up
-					</Typography>
-					<Form
-						onSubmit={this.onSubmit}
-						initialValues={{ employed: true, stooge: 'larry' }}
-						render={({ handleSubmit }) => (
-							<form className={classes.form}>
-								<Field
-									name="name"
-									component={FieldText}
-									type="text"
-									label="Full name"
-								/>
-								<Field
-									name="email"
-									component={FieldText}
-									type="text"
-									label="Email"
-								/>
-								<Field
-									name="password"
-									component={FieldText}
-									type="text"
-									label="Password"
-								/>
-								<Link to="/login">
-									Login
-								</Link>
-								<Button
-									fullWidth
-									onClick={() => handleSubmit()}
-									variant="contained"
-									color="primary"
-									className={classes.submit}
-								>
-									Sign in
-								</Button>
-							</form>
-						)}
-					/>
-				</Paper>
-			</main>
+			<SignUpPage
+				onSubmit={this.onSubmit}
+			/>
 		);
 	}
 }
 
-export default withStyles(styles)(SignUp);
+export default SignUp;
