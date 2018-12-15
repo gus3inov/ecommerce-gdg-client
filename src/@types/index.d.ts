70eec454e27c9583f9296fe7a3f9113f18e2cba0
Muslim Guseinov <gusejnov1999@list.ru>
2018-12-15 23:59:22 +0300

    update(typings)

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -6,4 +6,33 @@ type TypeRoute = {
 	exact: boolean;
 };
 
+type SignUpRequest = {
+	name: string;
+	email: string;
+	password: StringOrNumber;
+};
+
+type LoginRequest = {
+	email: string;
+	password: StringOrNumber;
+};
+
+type User = {
+	id: string;
+	email: string;
+	password: string;
+	name: string;
+};
+
+type Product = {
+	id: string;
+	name: string;
+	pictureUrl: string;
+	description: string;
+	price: number;
+	seller: User;
+};
+
+type Products = Product[];
+
 type StringOrNumber = string | number;
